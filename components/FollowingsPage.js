db0e4c338bd4e764c311a31aaa289b6bffbec038
kiarashAlizadeh <97706913+kiarashAlizadeh@users.noreply.github.com>
2024-02-04 23:48:35 +0330

    followers and following page added

---
+++ components/FollowingsPage.js
@@ -0,0 +1,58 @@
+// Component for Followings Page
+export default {
+  content: async function () {
+    var id = window.location.toString().split('#')[1];
+
+    const title = 'Soccer Media | Followings';
+    document.title = title;
+
+    let userLogin;
+    let user;
+    let listData;
+    async function fetchAuth() {
+      const auth = await fetch('http://localhost:8080/api/auth', {
+        method: 'Get',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+      });
+      user = await auth.json();
+      if (auth.status === 500) {
+        userLogin = false;
+        window.location.href = '/';
+      } else if (auth.status === 200) {
+        userLogin = true;
+      }
+
+      // list of followers
+      const res = await fetch(`http://localhost:8080/api/followingList/${id}`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      });
+      listData = await res.json();
+      console.log('data:', listData);
+    }
+    await fetchAuth();
+
+    return `
+    <div class="container">
+    <div class="backBtnContainer">
+    <a class="backBtn" href="/profile">Back</a>
+    <div class="followings">
+    <h3>Followings</h3>
+    ${
+      listData.length >= 1
+        ? listData.map((user) => {
+            return `<div class="following"><img class="profileImage" src="../assets/images/profile.png" alt="user Profile" /> <span class="userDetails"><span>${
+              user.name + ' ' + user.familyName
+            }</span> <span>@${
+              user.userName
+            }</span></span> <button class="unFollowBtn">unFollow</button> </div>`;
+          })
+        : '<div>you have not follow any User </div>'
+    }
+    </div>
+    </div>
+    </div>
+    `;
+  },
+};
