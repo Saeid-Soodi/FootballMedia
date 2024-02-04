7916b23afabea00c9cbdd42f6c58fd5a8212302e
kiarashAlizadeh <97706913+kiarashAlizadeh@users.noreply.github.com>
2024-02-04 10:15:56 +0330

    new empty pages added with routes

---
+++ components/PredictPage.js
@@ -0,0 +1,30 @@
+// Component for not found Page
+export default {
+  content: async function () {
+    const title = 'Soccer Media | Predict';
+    document.title = title;
+
+    let userLogin;
+    let user;
+    async function fetchAuth() {
+      const auth = await fetch('http://localhost:8080/api/auth', {
+        method: 'Get',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+      });
+      user = await auth.json();
+      if (auth.status === 500) {
+        userLogin = false;
+      } else if (auth.status === 200) {
+        userLogin = true;
+      }
+    }
+    await fetchAuth();
+
+    return `
+    <div class="container">
+    Predict page
+    </div>
+    `;
+  },
+};
