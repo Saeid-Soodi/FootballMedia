c33a7f4a210bb0f37973484bd87b546f65627b24
Fatemeh ahmadi <f.ahmadiii.93@gmail.com>
2024-02-07 00:04:42 +0330

    edit setting page

--- components/SettingsPage.js
+++ components/SettingsPage.js
@@ -21,50 +21,89 @@ export default {
       }
     }
     await fetchAuth();
-
     return `
     <div class="mainSetting">
+      <img class="settingImage" src="./assets/images/settingImage.jpg" alt="" />
+      <img class="profileImage" src="./assets/images/profile.png" alt="" />
+
+ 
     <h3>Setting</h3>
     <div class="settingContainer">
-      <span>
+      <div>
+          <span>
+          <label>Name : </label>
+          <input type="text" id="nameInput" placeholder="Name"/>
+          </span>
+         
+          
+          <span>
+          <label>Phone Number : </label>
+          <input type="text" id="phoneInput" placeholder="Phone Number"/>
+          </span>
 
-      <label>Name : </label>
-      <input type="text" id="nameInput" placeholder="Name"/>
-      </span>
-      <span>
-      
-      <label>Family Name : </label>
-       <input type="text" id="FamilyNameInput" placeholder="Family Name"/>
-      </span>
-      <span>
+          <span>
 
-      <label>User Name : </label>
-      <input type="text" id="userNameInput" placeholder="User Name"/>
-      </span>
-      <span>
-      
-      <label>Phone Number : </label>
-      <input type="text" id="phoneInput" placeholder="Phone Number"/>
-      </span>
-      <span>
-      
-      <label>Email : </label>
-      <input type="email" id="emailInput" placeholder="Email"/>
-      </span>
-      <span>
-      
-      <label>Gender : </label>
-      <select
-            id="genderInput"
-          >
-            <option value=""  disabled selected>
-              Select Gender
-            </option>
-            <option value="MALE">Male</option>
-            <option value="FEMALE">Female</option>
-          </select>
-      </span>
+          <label>User Name : </label>
+          <input type="text" id="userNameInput" placeholder="User Name"/>
+
+          </span>
+
+          <span>
+            <label>Birth Date : </label>
+            <input type="date" id="birthDateInput"/>
+          </span>
+          
+          
       </div>
+      
+
+      
+      <div>
+          <span>
+              
+          <label>Family Name : </label>
+          <input type="text" id="FamilyNameInput" placeholder="Family Name"/>
+          </span>
+         
+          <span>
+          
+          <label>Email : </label>
+          <input type="email" id="emailInput" placeholder="Email"/>
+          </span>
+
+          <span>
+          
+          <label>Favorite Team: </label>
+          <select
+                id="favoriteTeamInput"
+              >
+                <option value=""  disabled selected>
+                  Select your Favorite Team
+                </option>
+                <option value="65c1fe6be3d6499b5031b39e">Chelsea FC</option>
+                <option value="65c2120026908c0b6257c183">FC Bayern Munich</option>
+                <option value="65c2138b26908c0b6257c18b">FC Barcelona</option>
+                <option value="65c2151d26908c0b6257c19c">Juventus FC</option>
+                <option value="65c2166226908c0b6257c1a3">Manchester United F.C.</option>
+                <option value="65c2186326908c0b6257c1aa">Real Madrid CF</option>
+              </select>
+          </span>
+          
+          <span>
+          
+          <label>Gender : </label>
+          <select
+                id="genderInput"
+              >
+                <option value=""  disabled selected>
+                  Select Gender
+                </option>
+                <option value="MALE">Male</option>
+                <option value="FEMALE">Female</option>
+              </select>
+          </span>
+      </div>
+    </div>
    
       <button class="settingBtn"  onclick="handleSignIn()">Change</button>
         <p>Do You want to Change Your Password ?  <a href="/changePassword">Click Here</a></p>
